62c82e5064a65bd1805ceb38a5def797aaf1c7d9
AntonChilikin <storik@mail.ru>
2024-01-30 10:41:57 +0800

    dz7

--- packer/scripts/install_ruby.sh
+++ packer/scripts/install_ruby.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 apt update
+while ps ax | grep -i [a]pt ; do sleep 10; done;
 apt install -y  ruby-full ruby-bundler build-essential
 
---
+++ terraform/prod/variables.tf
@@ -0,0 +1,43 @@
+variable "cloud_id" {
+  description = "Cloud"
+  default     = "b1gistq5i425ls9kfsi0"
+}
+variable "folder_id" {
+  description = "Folder"
+  default     = "b1g5s8c5d6g7q0dqggc0"
+}
+variable "zone" {
+  description = "Zone"
+  # Значение по умолчанию
+  default = "ru-central1-a"
+}
+
+variable "private_key_path" {
+  # Описание переменной
+  description = "Path to the private key used for ssh access"
+}
+
+variable "public_key_path" {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+
+}
+variable "image_id" {
+  description = "Disk image"
+  default     = "fd871qpek9tslm8vvd3e"
+}
+variable "subnet_id" {
+  description = "Subnet"
+  default     = "e9b2bfnugdh9cj9qgu53"
+}
+variable "service_account_key_file" {
+  description = "key .json"
+}
+variable "app_disk_image" {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+variable "db_disk_image" {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
